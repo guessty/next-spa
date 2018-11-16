c76fc3ae4e5e575aa35f5d5de9d25bdc67c13ec0
Christian <christian.guest@outlook.com>
2018-11-16 23:28:25 +0000

    more clean up of store removal

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { default as Server } from './lib/build/server'
 
 export { default as Link } from './link'
 export { default as Router, withSPARouter } from './router'
-export { Provider, Container, subscribe } from './store'
 export { default as withSPA } from './lib/build/config'
 
 export default Server
