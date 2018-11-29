4442c45c7cc6ca5321e33dc6aabddb95ec0f8680
knowmorejs <christguest@hotmail.com>
2018-11-29 17:03:22 +0000

    exposed getRewrites

---
+++ src/lib/build/deployment/getRewrites.ts
@@ -0,0 +1,36 @@
+import * as path from 'path'
+
+export const getDynamicSource = (route: any) => {
+  const splitRoute = route.split('/')
+  return splitRoute.map((element: any) => {
+    return element.includes(':') ? '**' : element
+  }).join('/')
+}
+
+export default async (routes: any, config: any) => {
+  const nextSPAConfig = config.nextSPA || {}
+
+  const staticRewrites = routes.filter((route: any) =>
+    !route.pattern.includes('/:')).map((route: any) => {
+      const source = route.pattern
+      return {
+        source,
+        destination: `${route.page === '/' ? '/index.html' : `${route.page}.html`}`,
+      }
+    })
+  
+  const dynamicRewrites = routes.filter((route: any) =>
+    route.pattern.includes('/:')).map((route: any) => {
+      const fallback = nextSPAConfig.fallback ? path.join('./', nextSPAConfig.fallback) : '/404.html'
+      const source = getDynamicSource(route.pattern)
+      return {
+        source,
+        destination: nextSPAConfig.exportDynamicPages ? `${route.page}.html` : fallback,
+      }
+    })
+
+  return {
+    staticRewrites,
+    dynamicRewrites,
+  }
+}
--- src/lib/build/deployment/serve.ts
+++ src/lib/build/deployment/serve.ts
@@ -1,49 +1,18 @@
 import { writeFileSync } from 'fs'
 import * as path from 'path'
 //
-
-const getDynamicSource = (route: any) => {
-  const splitRoute = route.split('/')
-  return splitRoute.map((element: any) => {
-    return element.includes(':') ? '**' : element
-  }).join('/')
-}
-
-
-const getRewrites = async (routes: any, config: any) => {
-  const nextSPAConfig = config.nextSPA || {}
-
-  const staticRewrites = routes.filter((route: any) =>
-    !route.pattern.includes('/:')).map((route: any) => {
-      const source = route.pattern
-      return {
-        source,
-        destination: `${route.page === '/' ? '/index.html' : `${route.page}.html`}`,
-      }
-    })
-  
-  const dynamicRewrites = routes.filter((route: any) =>
-    route.pattern.includes('/:')).map((route: any) => {
-      const fallback = nextSPAConfig.fallback ? path.join('./', nextSPAConfig.fallback) : '/404.html'
-      const source = getDynamicSource(route.pattern)
-      return {
-        source,
-        destination: nextSPAConfig.exportDynamicPages ? `${route.page}.html` : fallback,
-      }
-    })
-
-  return [
-    ...staticRewrites,
-    ...dynamicRewrites,
-  ]
-}
+import getRewrites from './getRewrites'
+//
 
 export default async function buildServeDeploymentConfig (config: any, outdir?: string) {
   const routes = config.publicRuntimeConfig.routes
-  const serveRewrites = await getRewrites(routes, config)
+  const { staticRewrites, dynamicRewrites } = await getRewrites(routes, config)
   const serveJSONConfig = JSON.stringify({
     trailingSlash: true,
-    rewrites: serveRewrites
+    rewrites: [
+      ...staticRewrites,
+      ...dynamicRewrites,
+    ]
   }, null, 2)
 
   await writeFileSync(path.join(outdir, '/serve.json'), serveJSONConfig, 'utf8')
